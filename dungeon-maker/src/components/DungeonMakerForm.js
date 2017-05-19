202ba472de55284aac8d85acc2f81c722cb63431
Scott Gottreu <scott.gottreu@verizonwireless.com>
2017-05-19 16:28:47 -0500

    made DungeonMakerForm a stateless component

--- dungeon-maker/src/components/DungeonMakerForm.js
+++ dungeon-maker/src/components/DungeonMakerForm.js
@@ -1,41 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 
-class DungeonMakerForm extends Component {
-	
-	constructor(props){
-    super(props);
-    this.onChooseDungeon = this.props.onChooseDungeon.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-
-  }
+const DungeonMakerForm = ({ onChooseDungeon, onSaveDungeonGrid, foundDungeonGrids, selectedDungeon }) => {
 
 	handleChange(event, index, value){
-		this.onChooseDungeon(value);
+		onChooseDungeon(value);
 	}
 
-	render(){
-		let {onSaveDungeonGrid, foundDungeonGrids, selectedDungeon} = this.props;
+	return (
+		<div className="DungeonMakerForm">
 
+			<TextField hintText="Encounter Name" />
+			<RaisedButton label="Save Encounter" primary={true}  onClick={onSaveDungeonGrid.bind(this)} />
 
-		return (
-			<div className="DungeonMakerForm">
-
-				<TextField hintText="Encounter Name" />
-	      <RaisedButton label="Save Encounter" primary={true}  onClick={onSaveDungeonGrid.bind(this)} />
-
-	      <SelectField onChange={this.handleChange} value={selectedDungeon} floatingLabelText="Saved Dungeon Grids" >
-          {foundDungeonGrids.map( (grid, x) => (
-          	<MenuItem key={grid._id} value={grid._id} primaryText={grid._id} />
-          ))}
-        </SelectField>
-	    </div>
-	  )
-	}
-
+			<SelectField onChange={this.handleChange} value={selectedDungeon} floatingLabelText="Saved Dungeon Grids" >
+				{foundDungeonGrids.map( (grid, x) => (
+					<MenuItem key={grid._id} value={grid._id} primaryText={grid._id} />
+				))}
+			</SelectField>
+		</div>
+	);
 }
 
 export default DungeonMakerForm;
