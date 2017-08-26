7dabd562f0e79f10d21e515384e09fdb31d13993
Scott Gottreu <sgottreu@gmail.com>
2017-08-26 12:38:06 -0500

    Added actions & container for Create Encounters

---
+++ dungeon-maker/src/api/encounters-api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {Variables} from '../lib/Variables';
+import store from '../store';
+import { loadAvailableEncounters } from '../actions/encounters-actions';
+
+export var findEncounters = () => {
+  return axios.get(`${Variables.host}/findEncounters`)
+	  .then(res => {
+	    store.dispatch(loadAvailableEncounters(res.data));
+	  }); 
+};
+export var saveParty = (party) => {
+  return axios.post(`${Variables.host}/saveParty`, party)
+	  .then(res => {
+			// let _state = store.getState();
+			// let availableEncounters = _state.encountersState.availableEncounters;
+			// let _i = availableEncounters.findIndex(function(m) { return m._id === res.data._id});
+			
+			// if(_i === -1) {
+			// 	availableEncounters.push( res.data );
+			// } else {
+			// 	availableEncounters[ _i ] = res.data;
+			// }
+			// store.dispatch(loadAvailableEncounters(availableEncounters));
+	  }); 
+};
+
